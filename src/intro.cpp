9d69a659dc5f2c843ce5471ad2afbd1ac19553d9
daniel <daniel@lub.myguest.virtualbox.org>
2023-06-07 14:49:16 +0200

    Introscreen

--- src/intro.cpp
+++ src/intro.cpp
@@ -5,7 +5,7 @@
 #include "gridfont.h"
 #include "sound.h"
 
-static int max_x = 0, max_y = 0; // screen size
+static float max_x = 0, max_y = 0; // screen size
 
 int intro()
 {
@@ -19,8 +19,8 @@ int intro()
    // print screen size
    // * LINES and COLS are predefined macros
    move(0, 0); // position cursor at origin
-   printw("screen size is %dx%d\n", COLS, LINES);
-   hline(ACS_HLINE, COLS);
+   //printw("screen size is %dx%d\n", COLS, LINES);
+   hline(ACS_HLINE, (COLS*0.9));
 
    // get screen size
    // * a preceding & as used by the call-by-ref convention is not required
@@ -35,22 +35,43 @@ int intro()
    draw_grid_text(ty - get_grid_char_lines()/2, tx - strlen(text)*get_grid_char_cols()/2, text);
 
    // print help text
-   const char help[] = "kuh drückne für rausgehe!";
+   const char help[] = "PRESS Q TO START";
    use_attr_bold(); // enable bold attribute
    use_attr_blink(); // enable blink attribute
    use_color(2); // index 2 equals red
-   mvprintw(max_y-2, 1, help);
+   mvprintw((max_y*0.6), (max_x*0.45) , help);
    use_color(); // default equals white
    use_attr_normal(); // disable all attributes
 
    // draw horizontal line
-   mvhline(max_y-3, 0, ACS_HLINE, max_x);
-
+   mvhline((max_y*0.85), 0, ACS_HLINE, (max_x*0.95));
+   mvhline((max_y*0.75), (max_x*0.2) , ACS_HLINE, max_x);
+   mvhline((max_y*0.95), 0, ACS_HLINE, (max_x*0.4));
+   mvhline((max_y*0.95), (max_x*0.5), ACS_HLINE, max_x);
+   mvhline((max_y*0.7), 0, ACS_HLINE, (max_x*0.3));
+   mvhline((max_y*0.15), (max_x*0.1) , ACS_HLINE, max_x);
+   mvhline((max_y*0.05), 0, ACS_HLINE, (max_x*0.5));
+   mvhline((max_y*0.05), (max_x*0.6), ACS_HLINE, max_x);
+   mvhline((max_y*0.2), 0, ACS_HLINE, (max_x*0.4));
+   mvhline((max_y*0.3), (max_x*0.7) , ACS_HLINE, max_x);
+   
+   
+   
+   //draw Stars
+   mvaddch((max_y*0.1),(max_x*0.15),'*');
+   mvaddch((max_y*0.23),(max_x*0.9),'+');
+   mvaddch((max_y*0.16),(max_x*0.45),'*');
+   mvaddch((max_y*0.18),(max_x*0.1),'+');
+   mvaddch((max_y*0.9),(max_x*0.2),'*');
+   mvaddch((max_y*0.82),(max_x*0.15),'+');
+   mvaddch((max_y*0.9),(max_x*0.85),'*');
+   mvaddch((max_y*0.72),(max_x*0.9),'+');
+   
    // terminal bell
    beep();
 
    // play WAV sound
-   sound_play("/home/vbox/ASCII-GFX-1.2/titlemusic.wav");
+   sound_play("titlemusic.wav",0.1);
 
    // refresh screen
    refresh();
